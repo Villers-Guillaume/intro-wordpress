da6e58c6ca8c971e7d99ba66f5bbb17269941295
Villers_Guillaume <guillaume.villers@student.hepl.be>
2025-02-14 14:41:01 +0100

    Added header and footer templates files

--- wp-content/themes/dw/front-page.php
+++ wp-content/themes/dw/front-page.php
@@ -1,28 +1,14 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title><?= wp_title('.', false, 'right') . get_bloginfo('name') ?></title>
-</head>
-<body>
-<header>
-    <h1><?= get_bloginfo('name') ?></h1>
-    <p><?= get_bloginfo('description') ?></p>
-</header>
-
-<main>
-    <h2>Bienvenue sur mon site&nbsp;!</h2>
-    <?php
-    //On ouvre la boucle (the loop) la structure de contrôle de contenu propre à wordpress
-    if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <h2><?= get_the_title();?></h2>
-        <div class="wysiwyg"><?= get_the_content(); ?></div>
-    <?php
+<?php get_header(); ?>
+<h2>Bienvenue sur mon site&nbsp;!</h2>
+<?php
+//On ouvre la boucle (the loop) la structure de contrôle de contenu propre à wordpress
+if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <h2><?= get_the_title(); ?></h2>
+    <div class="wysiwyg"><?= get_the_content(); ?></div>
+<?php
     //On ferme la boucle
-    endwhile; else: ?>
-        <p>Pas de contenu à afficher</p>
-    <?php endif; ?>
-</main>
-</body>
-</html>
+endwhile;
+else: ?>
+    <p>Pas de contenu à afficher</p>
+<?php endif; ?>
+<?php get_footer(); ?>
--- wp-content/themes/dw/index.php
+++ wp-content/themes/dw/index.php
@@ -1,27 +1,13 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title><?= wp_title('.', false, 'right') . get_bloginfo('name') ?></title>
-</head>
-<body>
-<header>
-    <h1><?= get_bloginfo('name') ?></h1>
-    <p><?= get_bloginfo('description') ?></p>
-</header>
-
-<main>
-    <?php
-    //On ouvre la boucle (the loop) la structure de contrôle de contenu propre à wordpress
-    if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <h2><?= get_the_title();?></h2>
-        <div class="wysiwyg"><?= get_the_content(); ?></div>
-    <?php
+<?php get_header(); ?>
+<?php
+//On ouvre la boucle (the loop) la structure de contrôle de contenu propre à wordpress
+if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <h2><?= get_the_title(); ?></h2>
+    <div class="wysiwyg"><?= get_the_content(); ?></div>
+<?php
     //On ferme la boucle
-    endwhile; else: ?>
-        <p>Pas de contenu à afficher</p>
-    <?php endif; ?>
-</main>
-</body>
-</html>
+endwhile;
+else: ?>
+    <p>Pas de contenu à afficher</p>
+<?php endif; ?>
+<?php get_footer(); ?>
