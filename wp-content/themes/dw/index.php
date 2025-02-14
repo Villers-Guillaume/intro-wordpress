e0c9190313158e2196f52fe86010486c54a7e6fb
Villers_Guillaume <guillaume.villers@student.hepl.be>
2025-02-14 14:19:12 +0100

    Added "the loop"

--- wp-content/themes/dw/index.php
+++ wp-content/themes/dw/index.php
@@ -6,7 +6,22 @@
     <title><?= wp_title('.', false, 'right') . get_bloginfo('name') ?></title>
 </head>
 <body>
-<h1><?= get_bloginfo('name') ?></h1>
-<p><?= get_bloginfo('description') ?></p>
+<header>
+    <h1><?= get_bloginfo('name') ?></h1>
+    <p><?= get_bloginfo('description') ?></p>
+</header>
+
+<main>
+    <?php
+    //On ouvre la boucle (the loop) la structure de contrôle de contenu propre à wordpress
+    if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <h2><?= get_the_title();?></h2>
+        <div class="wysiwyg"><?= get_the_content(); ?></div>
+    <?php
+    //On ferme la boucle
+    endwhile; else: ?>
+        <p>Pas de contenu à afficher</p>
+    <?php endif; ?>
+</main>
 </body>
 </html>
